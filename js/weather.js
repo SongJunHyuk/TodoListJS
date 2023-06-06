7774541c1fd302bbebbece59cd12614ca3b2824b
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2023-06-06 23:43:20 +0900

    weather

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,14 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const API_KEY = "c18413b9d259934a99e2d628198f87da";
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
     const lon = position.coords.longitude;
+    console.log("You live in", lat, lon)
+
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    console.log(url);
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
